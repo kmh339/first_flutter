7acb16c1444081bfa49b074f680803177bf0befb
kmh339 <kmh339@gmail.com>
2019-12-10 14:29:02 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:english_words/english_words.dart' as prefix0;
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggstions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i){
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggstions.length) {
+            _suggstions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggstions[index]);
+    });
+  }
+
+  Widget _buildRow(prefix0.WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
